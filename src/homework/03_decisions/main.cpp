e0470d516670c6fd60b323247b326024b84612f8
arjunkc88 <157077755+arjunkc88@users.noreply.github.com>
2024-02-18 05:20:36 +0000

    Homework 3 if and switch

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,43 @@
 //write include statements
 
+#include <iostream>
+#include "decisions.h"
+
 int main() 
 {
-	return 0;
+    int choice;
+    int grade;
+
+    std::cout << "MAIN MENU\n"
+                 "1-Letter grade using if\n"
+                 "2-Letter grade using switch\n"
+                 "3-Exit\n"
+                 "Enter your choice: ";
+    std::cin >> choice;
+
+    switch (choice) 
+	{
+        case 1:
+            std::cout << "Enter numerical grade: ";
+            std::cin >> grade;
+            if (grade >= 0 && grade <= 100)
+                std::cout << "Letter grade using if: " << get_letter_grade_using_if(grade) << std::endl;
+            else
+                std::cout << "Number is out of range!" << std::endl;
+            break;
+        case 2:
+            std::cout << "Enter numerical grade: ";
+            std::cin >> grade;
+            if (grade >= 0 && grade <= 100)
+                std::cout << "Letter grade using switch: " << get_letter_grade_using_switch(grade) << std::endl;
+            else
+                std::cout << "Number is out of range!" << std::endl;
+            break;
+        case 3:
+            return 0;
+        default:
+            std::cout << "Invalid choice!" << std::endl;
+    }
+
+    return 0;
 }
