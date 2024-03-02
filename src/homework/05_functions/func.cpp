cd1a08fe523dcd4b99f23dd9849bfe27959cb9be
arjunkc88 <157077755+arjunkc88@users.noreply.github.com>
2024-03-02 16:24:08 +0000

    Homework 5 Dna test

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,42 @@
-//add include statements
+#include "func.h"//add include statements
 
 //add function code here
+double get_gc_content(const std::string& dna)
+ {
+    int count = 0;
+    for(char nucleotide : dna) {
+        if(nucleotide == 'C' || nucleotide == 'G') 
+        {
+            count++;
+        }
+    }
+    return static_cast<double>(count) / dna.size();
+}
+
+// Function to reverse a DNA string
+std::string get_reverse_string(std::string dna)
+ {
+    std::string reversed;
+    for(int i = dna.size() - 1; i >= 0; i--) 
+    {
+        reversed += dna[i];
+    }
+    return reversed;
+}
+
+// Function to get the DNA complement
+std::string get_dna_complement(std::string dna)
+ {
+    dna = get_reverse_string(dna); 
+    for(char& nucleotide : dna) 
+    {
+        switch(nucleotide) 
+        {
+            case 'A': nucleotide = 'T'; break;
+            case 'T': nucleotide = 'A'; break;
+            case 'C': nucleotide = 'G'; break;
+            case 'G': nucleotide = 'C'; break;
+        }
+    }
+    return dna;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,32 @@
+#include "func.h"
+#include <iostream>
 
 int main() 
 {
+    int choice;
+    std::string dna;
+    do {
+        std::cout << "Menu\n1- Get GC Content\n2- Get DNA Complement\n3- Exit\nEnter your choice: ";
+        std::cin >> choice;
+        switch (choice) 
+        {
+            case 1:
+                std::cout << "Enter DNA string: ";
+                std::cin >> dna;
+                std::cout << "GC Content: " << get_gc_content(dna) << "%\n";
+                break;
+            case 2:
+                std::cout << "Enter DNA string: ";
+                std::cin >> dna;
+                std::cout << "DNA Complement: " << get_dna_complement(dna) << "\n";
+                break;
+            case 3:
+                std::cout << "Exiting...\n";
+                return 0;
+            default:
+                std::cout << "Invalid option. Please try again.\n";
+        }
+    } while (true);
 
-	return 0;
+    return 0;
 }
