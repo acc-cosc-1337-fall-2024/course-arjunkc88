3b191357d4ff9642750d91d6da6a696544aa6a7a
arjunkc88 <157077755+arjunkc88@users.noreply.github.com>
2024-04-16 04:01:33 +0000

    homework

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,33 +1,5 @@
-// #ifndef TIC_TAC_TOE_H 
-// #define TIC_TAC_TOE_H
 
-// #include <iostream>
-// #include <string>
-// #include <vector>
 
-// class TicTacToe {
-// public:
-//     void start_game(std::string first_player);
-//     void mark_board(int position);
-//     bool game_over();
-//     std::string get_player() const;
-//     void display_board() const;
-//     std::string get_winner() const;
-
-// private:
-//     bool check_column_win();
-//     bool check_row_win();
-//     bool check_diagonal_win();
-//     void set_winner();
-//     void set_next_player();
-//     bool check_board_full() const;
-//     void clear_board();
-//     std::string player;
-//     std::vector<std::string> pegs{9, " "};
-//     std::string winner;
-// };
-
-// #endif // TIC_TAC_TOE_H
 #ifndef TIC_TAC_TOE_H
 #define TIC_TAC_TOE_H
 
